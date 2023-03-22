693d9886347080528da82c386ddd043d85955977
Softtery <74971137+Softtery@users.noreply.github.com>
2023-03-22 15:26:59 +0300

    Delete files directory

--- files/js/menu.js
+++
@@ -1,7 +0,0 @@
-$('.menu-btn').on('click', function(e) {
-  e.preventDefault();
-  $('.b-header').toggleClass('b-header_active');
-  $('.menu').toggleClass('menu_active');
-  $('.content').toggleClass('content_active');
-  $('.b-footer').toggleClass('b-footer_active');
-})
